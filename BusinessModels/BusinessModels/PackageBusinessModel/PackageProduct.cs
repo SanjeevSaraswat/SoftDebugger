e010548b1931c8bebf84d06a600d2ad930dc9c4c
SanjeevSaraswat <Sanjeev10Saraswat@Gmail.com>
2018-01-14 09:51:14 +0530

    Business Logic Sepratation

---
+++ BusinessModels/BusinessModels/PackageBusinessModel/PackageProduct.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackageBusinessModel.Models
+{
+   public class PackageProduct
+    {
+        public string CompanyID { get; set; }
+        public string ProductCode { get; set; }
+
+        public string ProductName { get; set; }
+    }
+}
