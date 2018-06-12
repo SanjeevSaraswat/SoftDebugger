59b3049461cdd3965a882d524d5774774aa64cf6
Sanjeev-Saraswat-IGT <Sanjeev.Saraswat@IGT.IN>
2018-06-12 19:08:47 +0530

    commit hotel costing file

---
+++ PackageModuleLayer/PackageModuleLayer/Models/PackageModel/ManagePackageHotelCosting.cs
@@ -0,0 +1,41 @@
+﻿using BusinessModels.PackageBusinessModel;
+using SoftLoggerAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackageModuleLayer.Models.PackageModel
+{
+  public  class ManagePackageHotelCosting
+    {
+        private AsyncLogger _logger = null;
+        public ManagePackageHotelCosting()
+        {
+            _logger = new AsyncLogger();
+            _logger.FileCollector = "Listener.Models.PackageModel.ManagePackageHotelCosting";
+            _logger.addMessage = new System.Collections.Specialized.NameValueCollection();
+        }
+
+        public bool UpdatePackageHotelCosting(HotelCosting objHotelCosting)
+        {
+
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            finally
+            {
+
+            }
+            return false;
+        }
+
+    }
+}
