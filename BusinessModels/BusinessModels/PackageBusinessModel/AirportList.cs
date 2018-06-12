ccc561f7877d032d0c515f0b387197c58554abde
Sanjeev-Saraswat-IGT <Sanjeev.Saraswat@IGT.IN>
2018-06-12 06:39:23 +0530

    commiy cost mgnt changes

---
+++ BusinessModels/BusinessModels/PackageBusinessModel/AirportList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessModels.PackageBusinessModel
+{
+   public class AirportList
+    {
+        public string CompanyID { get; set; }
+        public string AirportCode { get; set; }
+        public string AirportCity { get; set; }
+        public string AirportCountry { get; set; }
+
+        public string AirportText { get; set; }
+        public string Language { get; set; }
+        public string AirportName { get; set; }
+        public string query { get; set; }
+    }
+}
---
+++ BusinessModels/BusinessModels/PackageBusinessModel/PackageCostingBridgeModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessModels.PackageBusinessModel
+{
+    public class PackageCostingRequestBridgeModel
+    {
+        public string CompanyID { get; set; }
+
+        public string TokenID { get; set; }
+
+        public string PackageCode { get; set; }
+
+        public string PackageLanguage { get; set; }
+
+        public RequestTo RequestTo { get; set; }
+    }
+
+    public class HotelCosting
+    {
+        public string PackageCode { get; set; }
+
+        public string HotelName { get; set; }
+
+        public string HotelCity { get; set; }
+
+        public double AdultCost { get; set; }
+
+        public double ChildCost { get; set; }
+    }
+
+   public enum RequestTo
+    {
+        AddCosing,
+        UpdateCosting
+    }
+}
